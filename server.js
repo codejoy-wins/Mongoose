ff66367853ea9cd94d678541985130d6f4515ee6
Max <mjann@ucsc.edu>
2018-05-12 20:31:14 -0700

    no validations or timestamps

---
+++ server.js
@@ -0,0 +1,71 @@
+//requires
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var path = require('path');
+//connect to db
+mongoose.connect('mongodb://localhost/new_mongoose');
+// create schema
+
+var QuoteSchema = new mongoose.Schema({
+    name: String,
+    quote: String
+    })
+    mongoose.model('Quote', QuoteSchema);
+    var Quote = mongoose.model('Quote');
+
+//  ADD VALIDATIONS HERE ^^^
+// to make a model, you can first define a schema, which is just the BLUEPRINT for a model
+// var UserSchema = new mongoose.Schema({
+//     first_name:  { type: String, required: true, minlength: 6},
+//     last_name: { type: String, required: true, maxlength: 20 },
+//     age: { type: Number, min: 1, max: 150 },
+//     email: { type: String, required: true }
+// }, {timestamps: true });
+
+
+
+
+//uses
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, './static')));
+//sets
+app.set('views', path.join(__dirname, './views'));
+app.set('view engine', 'ejs');
+//routes
+app.get('/', function(req, res){
+    res.render('index');
+})
+
+app.get('/quotes', function(req, res){
+    Quote.find({}, function(err, quotes) {
+        if(err) {
+            console.log('something went wrong');
+        } else {
+            console.log('successfully added quote');
+            res.render('display', {stuff: quotes});
+        }
+    })
+})
+
+app.post('/quotes', function(req, res){
+    console.log(req.body);
+    var quote = new Quote({name: req.body.name, quote: req.body.quote});
+    quote.save(function(err) {
+        if(err){
+            console.log('mistake');
+        } else {
+            console.log('successfully added a quote');
+            res.redirect('/quotes');
+        }
+    })
+})
+
+
+
+
+
+app.listen(8000, function(){
+    console.log("listening on 8000");
+})
